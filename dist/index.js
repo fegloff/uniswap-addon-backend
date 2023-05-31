1ce417a9015ad7438aefc571971a4df36567828f
fegloff <fegloff@gmail.com>
2023-05-30 23:33:49 -0500

    add vercel configuration

--- dist/index.js
+++ dist/index.js
@@ -21,3 +21,32 @@ app.use(router);
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
+// {
+//   "name": "uniswap-back2",
+//   "version": "1.0.0",
+//   "description": "",
+//   "main": "index.js",
+//   "scripts": {
+//     "build": "npx tsc",
+//     "start": "node dist/index.js",
+//     "dev": "concurrently \"npx tsc --watch\" \"nodemon -q dist/index.js\""
+//   },
+//   "keywords": [],
+//   "author": "",
+//   "license": "ISC",
+//   "dependencies": {
+//     "axios": "^1.4.0",
+//     "cors": "^2.8.5",
+//     "dotenv": "^16.1.1",
+//     "express": "^4.18.2"
+//   },
+//   "devDependencies": {
+//     "@types/cors": "^2.8.13",
+//     "@types/express": "^4.17.17",
+//     "@types/node": "^20.2.5",
+//     "concurrently": "^8.0.1",
+//     "nodemon": "^2.0.22",
+//     "pre-commit": "^1.2.2",
+//     "rimraf": "^5.0.1"
+//   }
+// }
