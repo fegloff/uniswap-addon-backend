d871597ef77b84755e89ab8ce06fae0398dc183f
fegloff <fegloff@gmail.com>
2023-05-31 00:00:19 -0500

    update .gitignore

--- dist/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = void 0;
-exports.config = {
-    port: parseInt(process.env.PORT || "3001", 10),
-    uniswap: {
-        url: "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3",
-    },
-};
