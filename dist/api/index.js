bd2200f25163d8616d10f409e74c7836be323944
fegloff <fegloff@gmail.com>
2023-05-30 23:45:17 -0500

    vercel config changes

--- dist/api/index.js
+++ dist/api/index.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -15,9 +6,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.getPairPrice = void 0;
 const axios_1 = __importDefault(require("axios"));
 const config_1 = require("../config");
-function getPairPrice(pairAddress) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const query = `
+async function getPairPrice(pairAddress) {
+    const query = `
   {
     pool(id: "${pairAddress}") {
       tick
@@ -37,28 +27,27 @@ function getPairPrice(pairAddress) {
     }
   }
   `;
-        try {
-            // Make the API request
-            // const response = await fetch(config.uniswap.url, {
-            //   method: "POST",
-            //   headers: {
-            //     "Content-Type": "application/json",
-            //   },
-            //   body: JSON.stringify({
-            //     query: query,
-            //   }),
-            // });
-            const response = yield axios_1.default.post(config_1.config.uniswap.url, {
-                query: query,
-            });
-            console.log(response.data);
-            return response.data;
-        }
-        catch (error) {
-            console.error("Error:", error.message);
-            return null;
-        }
-    });
+    try {
+        // Make the API request
+        // const response = await fetch(config.uniswap.url, {
+        //   method: "POST",
+        //   headers: {
+        //     "Content-Type": "application/json",
+        //   },
+        //   body: JSON.stringify({
+        //     query: query,
+        //   }),
+        // });
+        const response = await axios_1.default.post(config_1.config.uniswap.url, {
+            query: query,
+        });
+        console.log(response.data);
+        return response.data;
+    }
+    catch (error) {
+        console.error("Error:", error.message);
+        return null;
+    }
 }
 exports.getPairPrice = getPairPrice;
 // const response = await axios.post(config.uniswap.url, {
--- dist/routes/uniswapRouter.js
+++ dist/routes/uniswapRouter.js
@@ -1,20 +1,11 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.uniswapRouter = void 0;
 const express_1 = require("express");
 const pairs_1 = require("../api/pairs");
 const api_1 = require("../api");
 exports.uniswapRouter = (0, express_1.Router)();
-exports.uniswapRouter.get("/pool/:pair", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+exports.uniswapRouter.get("/pool/:pair", async (req, res) => {
     const { pair } = req.params;
     console.log(pair);
     if (!pair)
@@ -22,6 +13,6 @@ exports.uniswapRouter.get("/pool/:pair", (req, res) => __awaiter(void 0, void 0,
     const id = (0, pairs_1.getPairId)(pair);
     if (!id)
         return res.sendStatus(400);
-    const data = yield (0, api_1.getPairPrice)(id);
+    const data = await (0, api_1.getPairPrice)(id);
     res.send({ data: data });
-}));
+});
